600357815d6643c7d3c942d0b601d05488bd963e
bobbyrosehag <bobbyrosehag@gmail.com>
2023-05-03 15:05:53 +0200

    Added credentials

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const Turn = require("node-turn");
 const server = new Turn({
   // set options
-  authMech: "none",
+  authMech: "long-term",
   debugLevel: "ALL",
+  credentials: {
+    username: "username",
+    password: "password",
+  },
 });
 
 server.start();
